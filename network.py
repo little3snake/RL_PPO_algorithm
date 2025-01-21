e4ec765c26c000549c7bd27a9755e22023588c33
Tanya Subbotina <146174173+little3snake@users.noreply.github.com>
2025-01-21 23:12:08 +0300

    Update2 network.py

--- network.py
+++ network.py
@@ -1,24 +1,35 @@
-"""
-  This file contains a neural network module for us to
-  define our actor and critic networks in PPO.
-"""
 import torch
-from torch import nn
+import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
 
-class NN(nn.Module):
-  def __init__(self, input_dim, output_dim):
-    super(NN, self).__init__()
-    self.layer1 = nn.Linear(input_dim, 64)
-    self.layer2 = nn.Linear(64, 64)
-    self.layer3 = nn.Linear(64, output_dim)
+class Actor(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(Actor, self).__init__()
+        self.layer1 = nn.Linear(input_dim, 64)
+        self.layer2 = nn.Linear(64, 64)
+        self.layer3 = nn.Linear(64, output_dim)
+        # Инициализация весов
+        nn.init.xavier_uniform_(self.layer1.weight)
+        nn.init.xavier_uniform_(self.layer2.weight)
+        nn.init.xavier_uniform_(self.layer3.weight)
 
-  def forward(self, n_observations):
-    #if isinstance(n_observations, np.ndarray):
-    #  n_observations = torch.tensor(n_observations, dtype=torch.float, device=device)
-    #else:
-    #  n_observations = n_observations.to(device)
-    x = F.relu(self.layer1(n_observations))
-    x = F.relu(self.layer2(x))
-    return self.layer3(x)
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
+
+class Critic(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(Critic, self).__init__()
+        self.layer1 = nn.Linear(input_dim, 64)
+        self.layer2 = nn.Linear(64, 64)
+        self.layer3 = nn.Linear(64, output_dim)
+        # Инициализация весов
+        nn.init.xavier_uniform_(self.layer1.weight)
+        nn.init.xavier_uniform_(self.layer2.weight)
+        nn.init.xavier_uniform_(self.layer3.weight)
+
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
