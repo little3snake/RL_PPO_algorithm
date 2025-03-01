c78a91fa9b3ac40ac5f24ca59853e928d1a48949
Tanya Subbotina <146174173+little3snake@users.noreply.github.com>
2025-03-01 22:20:38 +0300

    Update4 network.py

--- network.py
+++ network.py
@@ -10,18 +10,11 @@ import numpy as np
 class NN(nn.Module):
   def __init__(self, input_dim, output_dim):
     super(NN, self).__init__()
-    self.layer1 = nn.Linear(input_dim, 64)
-    self.layer2 = nn.Linear(64, 64)
-    self.layer3 = nn.Linear(64, output_dim)
-    #self.layer4 = nn.Linear(64, output_dim)
+    self.layer1 = nn.Linear(input_dim, 128)
+    self.layer2 = nn.Linear(128, 128)
+    self.layer3 = nn.Linear(128, output_dim)
 
   def forward(self, n_observations):
-    #if isinstance(n_observations, np.ndarray):
-    #  n_observations = torch.tensor(n_observations, dtype=torch.float, device=device)
-    #else:
-    #  n_observations = n_observations.to(device)
-    # nn.Tahn()
-    x = F.relu(self.layer1(n_observations))
-    x = F.relu(self.layer2(x))
-    #x = F.relu(self.layer3(x))
+    x = F.tanh(self.layer1(n_observations))
+    x = F.tanh(self.layer2(x))
     return self.layer3(x)
