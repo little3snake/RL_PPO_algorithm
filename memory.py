408459f8fd08cbd5261e611a34467b779f53f99d
Tanya Subbotina <146174173+little3snake@users.noreply.github.com>
2025-03-01 22:21:05 +0300

    Update2 memory.py

--- memory.py
+++ memory.py
@@ -2,13 +2,14 @@ import torch
 
 class Memory:
 
-    def __init__(self, obs_size, action_size, **kwargs):
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    def __init__(self, device, obs_size, action_size, **kwargs):
+        #self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.device = device
         self.obs_size = obs_size
         self.action_size = action_size
         print("Memory kwargs:", kwargs)
 
-        self.batch_size = kwargs.get('batch_size', 64)
+        self.batch_size = kwargs.get('timesteps_per_batch', 64)
         #print ("batch_size", self.batch_size)
         self.gamma = kwargs.get('gamma', 0.99)
         self.gae_lambda = kwargs.get('gae_lambda', 0.95)
@@ -20,14 +21,14 @@ class Memory:
         self.next_observations = torch.zeros(self.batch_size, self.obs_size).to(self.device) # for disc_rew
         self.rewards = torch.zeros(self.batch_size).to(self.device)
         self.dones = torch.zeros(self.batch_size).to(self.device)
-        self.advantages = torch.zeros(self.batch_size + 1).to(self.device)
+        self.advantages = torch.zeros(self.batch_size).to(self.device)
         self.discounted_rewards = torch.zeros(self.batch_size + 1).to(self.device)
         self.V = torch.zeros(self.batch_size).to(self.device) # values form critic -- old_value_state
 
     def add(self, obs, action, logprob, new_obs, reward, done, current_step):
         self.observations[current_step] = torch.Tensor(obs).to(self.device)
         self.actions[current_step] = torch.Tensor(action).to(self.device)
-        self.logprobs[current_step] = logprob
+        self.logprobs[current_step] = logprob.detach()
 
         #                            HOW
         self.next_observations[current_step] = torch.Tensor(new_obs).to(self.device)
@@ -37,8 +38,18 @@ class Memory:
     def set_V (self, V):
         self.V = V
 
-    def set_gae_advantages (self, V_next):
+    def get_logprobs(self):
+        return self.logprobs
+    def get_advantages(self):
+        return self.advantages
+    def get_discounted_rewards(self):
+        return self.discounted_rewards
+
+    def set_gae_advantages (self, V_s, V_next):
         # advantages in memory - discounted rewards too
+        #V_next.detach()
+        #self.V.detach()
+
         self.discounted_rewards[self.batch_size] = V_next[-1]
         last_advantage = 0
         last_value = V_next[-1]
@@ -46,7 +57,8 @@ class Memory:
             mask = 1.0 - self.dones[t]
             last_value = last_value * mask
             last_advantage = last_advantage + mask
-            delta = self.rewards[t] + self.gamma * last_value - self.V[t]
+            #delta = self.rewards[t] + self.gamma * last_value - self.V[t]
+            delta = self.rewards[t] + self.gamma * last_value - V_s[t]
             last_advantage = delta + self.gamma * self.gae_lambda * last_advantage
             self.advantages[t] = last_advantage
             last_value = V_next[t]
@@ -62,7 +74,7 @@ class Memory:
         self.next_observations = torch.zeros(self.batch_size, self.obs_size).to(self.device)  # for disc_rew
         self.rewards = torch.zeros(self.batch_size).to(self.device)
         self.dones = torch.zeros(self.batch_size).to(self.device)
-        self.advantages = torch.zeros(self.batch_size + 1).to(self.device)
+        self.advantages = torch.zeros(self.batch_size).to(self.device)
         self.discounted_rewards = torch.zeros(self.batch_size + 1).to(self.device)
         self.V = torch.zeros(self.batch_size).to(self.device)  # values form critic
 
