9d460e56919095fe045bc3eae9b2b1c197072848
Tanya Subbotina <146174173+little3snake@users.noreply.github.com>
2025-03-02 13:34:25 +0300

    Update7 ppo.py не раб код, но исправленный rollout

--- ppo.py
+++ ppo.py
@@ -20,7 +20,7 @@ class PPO:
         #print (kwargs)
 
         #self.total_timesteps = kwargs.get('total_timesteps', 100_000)
-        self.timesteps_per_batch = kwargs.get('timesteps_per_batch', 2048)
+        self.timesteps_per_batch = kwargs.get('timesteps_per_batch', 4800)
         self.max_timesteps_per_episode = kwargs.get('max_timesteps_per_episode', 1600)
         self.n_updates_per_iteration = kwargs.get('n_updates_per_iteration', 20)
         self.learning_rate = kwargs.get('learning_rate', 0.000429)
@@ -29,12 +29,13 @@ class PPO:
         self.gae_lambda = kwargs.get('gae_lambda', 0.95)
         self.clip = kwargs.get('clip', 0.3598)  # 0.28
         self.save_freq = kwargs.get('update_epochs', 10)
-        self.num_minibatches = kwargs.get('num_minibatches', 4)
-        self.minibatch_size = self.timesteps_per_batch // self.num_minibatches # batch_size / num_minibatches
+        self.num_minibatches = kwargs.get('num_minibatches', 6)
+        self.minibatch_size = 0 # batch_size / num_minibatches  - init in rollout - end
         self.max_grad_norm = kwargs.get('max_grad_norm', 0.5)
         self.entropy_coef = kwargs.get('entropy_coef', 0.01)
         self.clip_epsilon = kwargs.get('clip_epsilon', 0.2)
         self.num_iterations = kwargs.get('num_iterations', 400)
+        self.num_episodes_in_rollout = self.timesteps_per_batch // self.max_timesteps_per_episode
 
         self.env = env
         self.n_observations = env.observation_space.shape[0]
@@ -52,6 +53,7 @@ class PPO:
 
         self.memory = Memory(device=self.device, obs_size=self.env.observation_space.shape[0], action_size=self.env.action_space.shape[0],
                              **kwargs)
+        self.timesteps_in_current_batch = 0 # init in rollout (in the end)
 
         # This logger will help us with printing out summaries of each iteration
         self.logger = {
@@ -91,8 +93,8 @@ class PPO:
             iterations_current += 1
             self.logger['timesteps_current'] = timesteps_current
             self.logger['iterations_current'] = iterations_current
-            #self.writer.add_scalar("charts/current_step", int(time.time() - start_time), timesteps_current)
-            #self.writer.add_scalar("charts/current_iteration", int(time.time() - start_time), iterations_current)
+            self.writer.add_scalar("charts/current_step", int(time.time() - start_time), timesteps_current)
+            self.writer.add_scalar("charts/current_iteration", int(time.time() - start_time), iterations_current)
 
             counting_start = time.time()
             #V,_,_ = self.evaluate(batch_observations, batch_actions)
@@ -106,14 +108,20 @@ class PPO:
             self.logger['counting_time'] = counting_end - counting_start
 
             weights_update_start = time.time()
-            batch_inds = np.arange(self.timesteps_per_batch)
+            batch_inds = np.arange(self.timesteps_in_current_batch)
             for _ in range(self.n_updates_per_iteration):  # ALG STEP 6 & 7
                 np.random.shuffle(batch_inds)
-                for i in range (0, self.timesteps_per_batch, self.minibatch_size):
+                for i in range (0, self.timesteps_in_current_batch, self.minibatch_size):
                 #V, current_log_probs, entropy = self.evaluate(batch_observations, batch_actions)
+                    #print ("current_batch_size", self.timesteps_in_current_batch)
+                    #print("current_minibatch_size", self.minibatch_size)
+
                     start = i
                     finish = i + self.minibatch_size
                     minibatch_inds = batch_inds[start:finish]
+                    if len(minibatch_inds) < 2:
+                        break
+                    #print ("minibatch_inds", minibatch_inds)
                     V_evaluate, current_log_probs, entropy = self.evaluate(self.memory.observations[minibatch_inds], self.memory.actions[minibatch_inds],)
 
                 #ratios = torch.exp(current_log_probs - batch_log_probs)
@@ -125,6 +133,7 @@ class PPO:
                 #clip_loss_2part = torch.clamp(ratios, 1 - self.clip, 1 + self.clip) * A_k
 
                     A_k = self.memory.get_advantages()[minibatch_inds]#.clone()#.detach()
+                    #print("A_k ", A_k)
                 #print (self.memory.get_discounted_rewards()[0:self.timesteps_per_batch].shape)
                     A_k_norm = (A_k - A_k.mean()) / (A_k.std() + 1e-8)
 
@@ -148,8 +157,8 @@ class PPO:
                     self.critic_optim.step()
 
                     self.logger['actor_losses'].append(actor_loss.detach().cpu())
-                    #self.writer.add_scalar("losses/actor_loss", actor_loss.item(), timesteps_current)
-                    #self.writer.add_scalar("losses/critic_loss", critic_loss.item(), timesteps_current)
+                    self.writer.add_scalar("losses/actor_loss", actor_loss.item(), timesteps_current)
+                    self.writer.add_scalar("losses/critic_loss", critic_loss.item(), timesteps_current)
                 #self.writer.add_scalar("charts/reward", reward, timesteps_current)
 
             weights_update_end = time.time()
@@ -161,7 +170,7 @@ class PPO:
                 torch.save(self.actor.state_dict(), './ppo_actor.pth')
                 torch.save(self.critic.state_dict(), './ppo_critic.pth')
 
-            #self.memory.clear()
+            self.memory.clear()
 
         # Generate gif after learning
         obs, _ = self.env.reset()
@@ -191,23 +200,29 @@ class PPO:
         #episode_rewards, last_episode_rewards = [], [] # last only for writer
         batch_len = 0
         episode_rewards = []
+        done = False
+        current_timestep = -1 # for 0 in first step (because of +1)
 
-        for current_timestep in range(self.timesteps_per_batch):
-            timestep_start = current_timestep
-            batch_len += 1
+        #for current_timestep in range(self.timesteps_per_batch):
+        #for current_episode in range(self.num_episodes_in_rollout):
+        while current_timestep < self.timesteps_per_batch:
+            #timestep_start = current_timestep
+            while not done:
+                batch_len += 1
+                current_timestep += 1
 
             #with torch.no_grad():
-            action, log_prob = self.get_action(observation)
-            next_observation, reward, terminated, truncated, _ = self.env.step(action)
+                action, log_prob = self.get_action(observation)
+                next_observation, reward, terminated, truncated, _ = self.env.step(action)
             #next_observation = torch.tensor(observation, dtype=torch.float, device=self.device)
-            done = terminated or truncated
+                done = terminated or truncated
             #if done:
             #    done = 1
             #else:
             #    done = 0
             #done = torch.tensor(done, dtype=torch.int, device=self.device)
-            episode_rewards.append(reward)  # for disc_rew
-            self.add_to_memory(observation, action, log_prob, next_observation, reward, done, current_timestep)
+                episode_rewards.append(reward)  # for disc_rew
+                self.add_to_memory(observation, action, log_prob, next_observation, reward, done, current_timestep)
             #batch_observations.append(observation)
             #batch_actions.append(action)
             #batch_log_probs.append(log_prob)
@@ -215,23 +230,30 @@ class PPO:
             #reward = torch.tensor(reward, dtype=torch.float, device=self.device)
             #batch_rewards.append(reward) # for gae
             #batch_dones.append(done)
-            observation = next_observation
+                observation = next_observation
             #observation = torch.tensor(observation, dtype=torch.float, device=self.device)
 
-            if done:
+            '''if done:
                 #print ("in done timestep", current_timestep)
                 batch_lens.append(len(episode_rewards))
                 batch_rews.append(episode_rewards)
                 #last_episode_rewards = episode_rewards
                 episode_rewards = []
                 observation, _ = self.env.reset()
-                #observation = torch.tensor(observation, dtype=torch.float, device=self.device)
+                #observation = torch.tensor(observation, dtype=torch.float, device=self.device)'''
+            batch_lens.append(len(episode_rewards))
+            batch_rews.append(episode_rewards)
+            # last_episode_rewards = episode_rewards
+            episode_rewards = []
+            observation, _ = self.env.reset()
+            done = False
+            # observation = torch.tensor(observation, dtype=torch.float, device=self.device)
 
             #batch_lens.append(len(episode_rewards))
             #batch_rews.append(episode_rewards)
-        if episode_rewards != []:
+        '''if episode_rewards != []:
             batch_rews.append(episode_rewards)
-            batch_lens.append(len(episode_rewards))
+            batch_lens.append(len(episode_rewards))'''
 
         #batch_observations = torch.tensor(batch_observations, dtype=torch.float).to(self.device)
         #batch_next_observations = torch.stack(batch_next_observations).to(self.device)
@@ -242,6 +264,10 @@ class PPO:
         #batch_rewards = torch.stack(batch_rewards).to(self.device)
         #batch_discounted_rewards = self.compute_discounted_rewards(batch_rews).to(self.device)
 
+        self.memory.finalize()
+        self.timesteps_in_current_batch = self.memory.get_current_batch_size()
+        self.minibatch_size = self.timesteps_in_current_batch // self.num_minibatches  # batch_size / num_minibatches
+
         self.logger['batch_rewards'] = batch_rews
         self.logger['batch_lens'] = batch_lens
 
@@ -249,8 +275,8 @@ class PPO:
 
         avg_episode_rewards = np.mean([np.sum(episode_rewards) for episode_rewards in self.logger['batch_rewards']])
         avg_episode_lens = np.mean(self.logger['batch_lens'])
-        #self.writer.add_scalar("charts/avg_episode_rewards", avg_episode_rewards, timestep_current)
-        #self.writer.add_scalar("charts/avg_episode_lens", avg_episode_lens, timestep_current)
+        self.writer.add_scalar("charts/avg_episode_rewards", avg_episode_rewards, timestep_current)
+        self.writer.add_scalar("charts/avg_episode_lens", avg_episode_lens, timestep_current)
 
         #return batch_observations, batch_actions, batch_log_probs, batch_next_observations, batch_dones, batch_rewards, batch_discounted_rewards, batch_lens
         return batch_len
