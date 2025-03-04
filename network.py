101a1abf7a0f9a84c0e10d987ad9d822c6c0c637
Tanya Subbotina <146174173+little3snake@users.noreply.github.com>
2025-03-04 20:04:17 +0300

    Update3 network.py не работает

--- network.py
+++ network.py
@@ -6,19 +6,18 @@ import torch.distributed as dist
 class NN(nn.Module):
   def __init__(self, input_dim, output_dim):
     super(NN, self).__init__()
-    self.layer1 = nn.Linear(input_dim, 64)
-    self.layer2 = nn.Linear(64, 64)
-    self.layer3 = nn.Linear(64, output_dim)
+    self.layer1 = nn.Linear(input_dim, 128)
+    self.layer2 = nn.Linear(128, 128)
+    self.layer3 = nn.Linear(128, output_dim)
 
   def forward(self, n_observations):
-    x = F.relu(self.layer1(n_observations))
-    x = F.relu(self.layer2(x))
+    x = F.leaky_relu(self.layer1(n_observations))
+    x = F.leaky_relu(self.layer2(x))
     return self.layer3(x)
 
   #def sync_parameters(self):
   #  for param in self.parameters():
   #    dist.broadcast(param.data, src=0)
-
 
 
 
