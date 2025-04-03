91362990c7c04382c2bd0a3678ce5047d7044c18
Tanya Subbotina <146174173+little3snake@users.noreply.github.com>
2025-04-03 15:34:55 +0300

    Update8 main.py

--- main.py
+++ main.py
@@ -29,18 +29,22 @@ class Args:
         self.wandb_entity = None # the name of the user/group
         #capture_gif = False
         self.capture_video = False
+        self.create_gif_bool = True
+        self.save_policies = False
         self.env_name = "BipedalWalker-v3"
-        self.total_timesteps = 1_500_000
+        self.total_timesteps = 3_500_000
         self.learning_rate = 0.0004755#0.0004755#87104 # * world_size
         self.anneal_lr = False
         self.gamma = 0.99421#0.988
         self.clip = 0.286#0.2566
         self.local_num_envs = 2 #2
-        self.num_minibatches = 4 # the number of minibatches for one updating
+        self.num_minibatches = 4#8 # the number of minibatches for one updating 4
         self.update_epochs = 14#14 # 4 - oridinally -- n_updates_per_iteration
         self.max_timesteps_per_episode = 1600
-        self.num_episodes_per_batch_in_one_process = 2
-        self.num_steps = self.max_timesteps_per_episode * self.num_episodes_per_batch_in_one_process #4096 // self.local_num_envs  # before updating - timesteps_per_batch / local_num_envs
+        self.num_episodes_per_batch_in_one_process = 1
+        #self.num_steps = 3200
+        self.batch_size = 6400#12800
+        #self.num_steps = self.max_timesteps_per_episode * self.num_episodes_per_batch_in_one_process #4096 // self.local_num_envs  # before updating - timesteps_per_batch / local_num_envs
         self.max_grad_norm: float = 0.5
         self.entropy_coef = 0.01
         self.gae_lambda = 0.95
@@ -51,7 +55,8 @@ class Args:
         #local_batch_size: int = 0
         #local_minibatch_size: int = 0
         self.num_envs: int = 0
-        self.batch_size: int = 0
+        self.num_steps: int = 0
+        #self.batch_size: int = 0
         #minibatch_size: int = 0
         self.num_iterations: int = 0
 
@@ -64,10 +69,12 @@ def init_distributed(args):
         dist.init_process_group(args.backend, rank=local_rank, world_size=args.world_size)
     else:
         warnings.warn("Running in non-distributed mode!")
+    args.num_envs = args.local_num_envs * args.world_size  # total number of envs
+    args.num_steps = args.batch_size // args.num_envs
     args.local_batch_size = int(args.local_num_envs * args.num_steps)  # for each process
     args.local_minibatch_size = int(args.local_batch_size // args.num_minibatches)  # for gradient descent
-    args.num_envs = args.local_num_envs * args.world_size  # total number of envs
-    args.batch_size = int(args.num_envs * args.num_steps)  # total batch size (total envs * num_steps_per_1_iter)
+    #args.num_envs = args.local_num_envs * args.world_size  # total number of envs
+    #args.batch_size = int(args.num_envs * args.num_steps)  # total batch size (total envs * num_steps_per_1_iter)
     args.minibatch_size = int(args.batch_size // args.num_minibatches)  # total minibatch size
     args.num_iterations = args.total_timesteps // args.batch_size
     return local_rank
@@ -147,6 +154,7 @@ if __name__ == "__main__":
 
     # Set unique seed per process
     args.seed += local_rank
+    #args.seed = args.seed + (local_rank * 10)
     random.seed(args.seed)
     np.random.seed(args.seed)  # for matrix - or in ppo.py
     torch.manual_seed(args.seed - local_rank)
@@ -164,7 +172,8 @@ if __name__ == "__main__":
     end_time = time.time()
     print (f"total time of {args.total_timesteps} steps in {args.world_size} processes: {(end_time - start_time):.4f}")
     if local_rank == 0:
-        #save_frames_as_gif(frames)
+        if args.create_gif_bool:
+            save_frames_as_gif(frames)
         print('total reward trained model =', total_reward)
 
     envs.close()
